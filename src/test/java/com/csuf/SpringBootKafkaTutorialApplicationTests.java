9dc4542e56331b1011198e74b63e430150d7a64f
Sanket Shahane <shahanesanket24@gmail.com>
2023-07-12 21:44:15 -0700

    First Commit

---
+++ src/test/java/com/csuf/SpringBootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
